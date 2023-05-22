af38d9596b1f5f64d02d5fadcd484aab341350b5
alexangria1992 <alex040892@gmail.com>
2023-05-22 22:52:22 +1200

    desktop footer styling

--- blogr-website/src/components/Footer.js
+++ blogr-website/src/components/Footer.js
@@ -7,12 +7,12 @@ const Footer = () => {
 
   return (
     <>
-      <footer className="footer text-center pb-2  ">
+      <footer className="footer text-center px-5 pb-1 md:grid md:grid-cols-2 md:px-10 lg:py-10 lg:text-left lg:px-10   ">
         <div>
-          <img src={logo} alt="" className="block mx-auto pt-10  " />
+          <img src={logo} alt="" className="block mx-auto pt-10 md:mx-0 " />
         </div>
 
-        <div>
+        <div className="md:grid md:grid-cols-2 lg:grid lg:grid-cols-3 footer-links  ">
           {footerLinks.map(({ id, title, links }) => (
             <ul key={id}>
               <h4 className="mt-10 text-lg py-3  ">{title}</h4>
